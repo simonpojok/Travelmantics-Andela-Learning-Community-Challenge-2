e5ef9db5483086cb224632f9ae741bd509ce2ef9
simonojok19 <simonojok19@gmail.com>
2019-08-03 16:14:24 +0300

    authentication is working well

--- app/src/main/java/com/example/travelmantics/AuthenticationActivity.java
+++ app/src/main/java/com/example/travelmantics/AuthenticationActivity.java
@@ -1,26 +1,104 @@
 package com.example.travelmantics;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
+import android.util.Log;
 
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.List;
+
 
 public class AuthenticationActivity extends AppCompatActivity {
     private static final int RC_SIGN_IN = 100;
-    Button signInButton;
-    FirebaseAuth firebaseAuth;
+    private final  String TAG = getClass().getSimpleName();
+    FirebaseUser user;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        if (firebaseAuth.getCurrentUser() != null){
-            // Start signed in activity
-            // startActivity(UserActivity.createIntent(this, null));
+
+        if ( user == null ){
+            List<AuthUI.IdpConfig> providers = Arrays.asList(
+                    new AuthUI.IdpConfig.EmailBuilder().build(),
+                    new AuthUI.IdpConfig.GoogleBuilder().build()
+            );
+
+            startActivityForResult(
+                    AuthUI.getInstance()
+                            .createSignInIntentBuilder()
+                            .setAvailableProviders(providers)
+                            .build(), RC_SIGN_IN
+            );
+
+        } else {
+            Log.d(TAG, "onCreate() user already signed in");
+            startActivity(UserActivity.createUserActivityIntent(this, user));
             finish();
         }
+
+
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN ){
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+            if (resultCode == RESULT_OK ) {
+                Log.d(TAG, "onActivityResult() Successfully signed in");
+                user = FirebaseAuth.getInstance().getCurrentUser();
+                startActivity(UserActivity.createUserActivityIntent(this, user));
+                finish();
+            } else {
+                Log.d(TAG, "failed");
+            }
+        }
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    public void signOut(){
+        AuthUI.getInstance()
+                .signOut(this)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    public void onComplete(@NonNull Task<Void> task) {
+                        Log.d(TAG, "signout");
+                    }
+                });
+    }
+
+    public static Intent createAuthenticationIntent(Context context){
+        Intent intent = new Intent();
+        intent.setClass(context, AuthenticationActivity.class);
+        return intent;
+    }
+
 }
--- app/src/main/java/com/example/travelmantics/UserActivity.java
+++ app/src/main/java/com/example/travelmantics/UserActivity.java
@@ -2,13 +2,46 @@ package com.example.travelmantics;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Objects;
+
 
 public class UserActivity extends AppCompatActivity {
+    private final String TAG = getClass().getSimpleName();
+    private static FirebaseUser currentFirebaseUser;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
+        Log.d(TAG, "onCreate()");
+        TextView userEmail = findViewById(R.id.user_email);
+        TextView userDisplayName = findViewById(R.id.user_display_name);
+
+        userEmail.setText(currentFirebaseUser.getEmail());
+        userDisplayName.setText(Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser()).getEmail());
+
+
+
     }
+
+    public static Intent createUserActivityIntent(Context context, FirebaseUser firebaseUser) {
+        Intent intent = new Intent();
+        intent.setClass(context, UserActivity.class);
+        currentFirebaseUser = firebaseUser;
+        return intent;
+    }
+
 }
